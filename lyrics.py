e75c847063d6b76ea7bec1ad2d25d36364039270
Divya Kumar Baid <78469660+DivyaKumarBaid@users.noreply.github.com>
2022-10-11 17:13:12 +0530

    Revert "Update lyrics.py"

--- lyrics.py
+++ lyrics.py
@@ -9,21 +9,36 @@ class GeniusAPI:
 
     def get_lyrics(self, song, artist = None):
         try:
+
             if artist is None:
                 song = self.genius.search_song(song)
+                
             else:
                 song = self.genius.search_song(song, artist)
+                
             return song.lyrics
-        
+
         except Exception as e:
             return e
     
-#Function to get current song
+
 def get_current_song():
     for file in os.listdir('./'):
         if file.endswith('.mp3'):
             return file.split('.mp3')[0]
 
-#Function to get song list       
 def get_song_list():
     return [file.split('.mp3')[0] for file in os.listdir('./') if file.endswith('.mp3')]
+    
+
+
+
+
+
+
+
+
+
+
+    
+
